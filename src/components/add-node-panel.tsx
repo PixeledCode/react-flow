1b30e71d78fc83b4df2b7e210970ef2184221e97
pixeledcode <ashoaib540918@gmail.com>
2023-05-20 19:58:13 +0530

    add message icon

--- src/components/add-node-panel.tsx
+++ src/components/add-node-panel.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { NodeTypes } from '../config/store'
+import { IconMessages } from '@tabler/icons-react'
 
 export const AddNodePanel = () => {
 	const onDragStart = (
@@ -17,13 +18,14 @@ export const AddNodePanel = () => {
 			</div>
 			<hr />
 
-			<div className="p-2 mt-3">
+			<div className="p-2 mt-3 w-fit min-w-[240px]">
 				<div
-					className="border border-blue-400 py-2 px-3 rounded-md cursor-pointer"
+					className="border border-blue-500 py-2 px-3 rounded-md cursor-pointer flex flex-col items-center gap-2"
 					onDragStart={(event) => onDragStart(event, 'textNode')}
 					draggable
 				>
-					Default Node
+					<IconMessages size="32" color="rgb(37,99,235)" />
+					<span className="font-semibold text-blue-600">Message</span>
 				</div>
 			</div>
 		</aside>
