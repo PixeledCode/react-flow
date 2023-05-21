edd1e3c5fac03535f8f75ae2701c480a9b022d8d
pixeledcode <ashoaib540918@gmail.com>
2023-05-21 11:24:00 +0530

    change brand text size

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -23,8 +23,10 @@ export function Header({ nodes }: { nodes: Node[] }) {
 	return (
 		<header className="py-2 px-4 shadow-sm flex justify-between gap-3 flex-wrap">
 			<div className="flex gap-2 items-center">
-				<Workflow size={32} />
-				<h1 className="text-3xl font-bold text-center ">Chatbot Flow</h1>
+				<Workflow size={24} />
+				<h1 className="text-xl font-bold text-center sm:text-2xl">
+					Chatbot Flow
+				</h1>
 			</div>
 			<Button size="sm" onClick={handleSaveClick}>
 				Save Changes
