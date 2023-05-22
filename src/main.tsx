8c1e4be037b592a8fb3d05f77d5938599930a59f
pixeledcode <ashoaib540918@gmail.com>
2023-05-22 09:56:43 +0530

    add content reload for pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './styles/globals.css'
+import { registerSW } from 'virtual:pwa-register'
+
+const updateSW = registerSW({
+	onNeedRefresh() {
+		if (confirm('New content available. Reload?')) {
+			updateSW(true)
+		}
+	},
+})
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
 	<React.StrictMode>
